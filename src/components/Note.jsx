1fefc882f7de110835dd96022366adb2f34cf2e8
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-03 18:52:12 +0530

    .note classname was given to the parent div of the Note.jsx to implement styles

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Note = () => {
-    return <div>
+    return <div class="note">
         <h1>This is the title of this Note</h1>
         <p>This is the body content of this node.</p>
     </div>
