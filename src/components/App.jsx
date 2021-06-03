e88ad39e5b83440e18d01e74632e6fa9c32ba80c
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-03 18:18:02 +0530

    App component was modified and the other components were layed

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Note from "./components/Note"
+
+const App = () => {
+    return <div>
+        <Header/>
+        <Note/>
+        <App/>
+        </div>
+}
+export default App
