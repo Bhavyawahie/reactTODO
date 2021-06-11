f215c8a3c655536179235dc8edb371ee0bea5871
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-12 02:35:47 +0530

    addition and deletion using state handing was created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import Header from "./Header"
 import CreateArea from "./CreateArea"
 import Note from "./Note"
 import Footer from "./Footer"
 
-// const noteInit = (note) => {
-//     return 
-// }
 
 const App = () => {
+    const [notes, setNotes] = useState([])
+    const noteInit = (note) => {
+        setNotes(prevVal => {
+            return [...prevVal, note]
+        });
+    }
+    const noteDel = (id) => {
+        setNotes(prevVal => {
+            return prevVal.filter((note, index) => {
+                        return index !==id
+                    })
+        })
+    }
+
     return <div>
         <Header/>
-        <CreateArea/>
-        <Note
-        key={1}
-        title="Note Title"
-        content="note.content"
-    />
+        <CreateArea onSubmit={noteInit}/>
+        {
+            notes.map((note, index) => {
+            return( 
+                <Note
+                    key={index}
+                    id={index} 
+                    title={note.title} 
+                    content={note.content}
+                    onDelClick={noteDel}
+                /> 
+                )
+            })
+        }
         <Footer/>
-        </div>
+    </div>
 }
 export default App
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,15 @@
 import React from "react"
 
 const Note = (props) => {
+    const delHandler = () => {
+        props.onDelClick(props.id)
+    }
+
     return ( 
     <div className="note">
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button>DELETE</button>
+        <button onClick={delHandler}>DELETE</button>
     </div>
     );
 }
