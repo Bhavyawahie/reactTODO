e0e9788cb25f8f2192ea6c1833ff6d4d69874043
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-12 01:13:08 +0530

    input states were handled

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react"
 
 const CreateArea = () => {
+    const [input, setInput] = useState({
+        title: "",
+        content: ""
+    });
+    const inputChangeHandler = (event) => {
+        const {name, value} = event.target;
+        setInput(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        });
+    }
 
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." id="" rows="3"/>
+                <input name="title" placeholder="Title" onChange={inputChangeHandler}/>
+                <textarea name="content" placeholder="Take a note..." onChange={inputChangeHandler} id="" rows="3"/>
                 <button>+</button>
             </form>
         </div>
