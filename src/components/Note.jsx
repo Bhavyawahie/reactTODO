8d036b840b4dd38bc0d2f9d565819f229e16a9ea
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-11 18:59:26 +0530

    Delete button feature added to notes component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,12 @@
 import React from "react"
 
 const Note = (prop) => {
-    return <div class="note">
+    return ( 
+    <div className="note">
         <h1>{prompt.title}</h1>
         <p>{prop.content}</p>
+        <button>Delete</button>
     </div>
+    );
 }
 export default Note
