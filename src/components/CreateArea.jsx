16abf8174c6545b68efa299bbb8369b21ca9c5d1
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-11 21:20:26 +0530

    component to create the note created and added to app

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+
+const CreateArea = () => {
+    return (
+        <div>
+            <form action="">
+            <input type="text" />
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <button></button>
+            </form>
+        </div>
+    )
+}
+
+export default CreateArea;
