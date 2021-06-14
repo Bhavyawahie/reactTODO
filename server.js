f672f631f75c3ef46e771858c7d908932b70138d
Bhavya Wahie <bhavyawahie@gmail.com>
2021-06-14 13:47:24 +0530

    error rectified in server.js

--- server.js
+++ server.js
@@ -19,45 +19,45 @@ const Note = mongoose.model("Note", noteSchema);
 
 app.route("/")
 
-.get((req, res) => {
-    Note.find({}, (err, foundNotes) => {
-        if(!err){
-            res.status(200).json(foundNotes);
-        }
-        else{
-            console.error(err);
-        }
-    });
-})
+    .get((req, res) => {
+        Note.find({}, (err, foundNotes) => {
+            if(!err){
+                res.status(200).json(foundNotes);
+            }
+            else{
+                console.error(err);
+            }
+        });
+    })
 
-.post((req, res) => {
-    const newNote = Note({
-        id: uuidv4();
-        title: req.body.title,
-        content: req.body.content
-    });
-    newNote.save((err) => {
-        if(err){
-            console.error(err);
-        }
-        else{
-            res.send("Note added successfully!");
-        }
-    });
+    .post((req, res) => {
+        const newNote = Note({
+            id: uuidv4();
+            title: req.body.title,
+            content: req.body.content
+        });
+        newNote.save((err) => {
+            if(err){
+                console.error(err);
+            }
+            else{
+                res.send("Note added successfully!");
+            }
+        });
 
-})
+    })
 
-.delete((req, res) => {
-    const id = req.body.id
-    Note.deleteOne({id: req.body.id}, (err) => {
-        if(err){
-            console.error(err);
-        }
-        else{
-            res.send("Article Deleted Successfully!");
-        }
+    .delete((req, res) => {
+        const id = req.body.id
+        Note.deleteOne({id: req.body.id}, (err) => {
+            if(err){
+                console.error(err);
+            }
+            else{
+                res.send("Article Deleted Successfully!");
+            }
+        });
     });
-});
 
 app.listen(4000, () => {
     console.log("Server started at http://localhost:4000")
