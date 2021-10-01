9ba63a4565cfb558237296b0fdb0c02fd7593726
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-02 02:26:43 +0530

    user model added

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema =  new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /[a-z0–9!#$%&’*+/=?^_`{|}~-]+(?:\.[a-z0–9!#$%&’*+/=?^_`{|}~-]+)*@(?:[a-z0–9](?:[a-z0–9-]*[a-z0–9])?\.)+[a-z0–9](?:[a-z0–9-]*[a-z0–9])?/
+    },
+})
+
+const User = mongoose.Model('User', userSchema)
+
+module.exports = {
+    User
+}
