5f9c22c58667badc5dfd01e8ea318fc089762782
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-09 22:29:29 +0530

    bcrypt, jwt, express-async handler added as dependecies, notes routes and controller created

---
+++ controllers/notesController.js
@@ -0,0 +1,25 @@
+const Notes = require('../models/noteModel')
+const {v4: uuidv4} = require('uuid')
+
+// desc:
+// path:
+// access: 
+
+
+const getNotes = () => {
+
+}
+
+const addNotes = () => {
+
+}
+
+const deleteNotes = () => {
+
+}
+
+module.exports = {
+    getNotes,
+    addNotes, 
+    deleteNotes
+}
