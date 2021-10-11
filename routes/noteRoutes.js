e734f09cb918d84fd4c4ed2600eb93c8156e6235
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-11 20:20:07 +0530

    auth middlware included with note controllers in note routes, minor errors in userModel and authmiddleware corrected

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router()
-const {getNotes, addNotes, deleteNotes} = require('../controllers/notesController')
+const {getNotes, addNotes, deleteNotes} = require('../controllers/notesController');
+const { protect } = require('../middlewares/authMiddleware');
 
-router.get("/", getNotes).post("/", addNotes)
-router.delete("/:id", deleteNotes )
+router.route("/").get(protect, getNotes).post(protect,addNotes)
+router.delete("/:id", protect, deleteNotes )
 
 module.exports = router
