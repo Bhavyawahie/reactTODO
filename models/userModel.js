5be4eac79285bef2d535adf0d3b268ac0f6972e3
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-11 18:14:37 +0530

    user model created and referenced with notes

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const userSchema =  new mongoose.Schema({
-    googleId: {
-        type: String,
-        required: true
-    },
     name: {
         type: String,
         required: true
@@ -12,8 +8,15 @@ const userSchema =  new mongoose.Schema({
     email: {
         type: String,
         required: true,
+        unique: true,
         match: /[a-z0–9!#$%&’*+/=?^_`{|}~-]+(?:\.[a-z0–9!#$%&’*+/=?^_`{|}~-]+)*@(?:[a-z0–9](?:[a-z0–9-]*[a-z0–9])?\.)+[a-z0–9](?:[a-z0–9-]*[a-z0–9])?/
     },
+    password: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
 })
 
 const User = mongoose.Model('User', userSchema)
