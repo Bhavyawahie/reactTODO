e3be8407b078d43686237af80017926029dd31d9
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-11 18:14:37 +0530

    user model created and referenced with notes

--- models/noteModel.js
+++ models/noteModel.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
     id: String,
-    // user:{
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     required: true,
-    //     ref: 'User'
-    // } ,
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    } ,
     title: {
         type: String,
         required: [true, "Title is required to create a new note!"]
