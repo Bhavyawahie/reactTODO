ce9f965dd838f368412e5a1d6b215c711ad76cbb
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-17 03:05:57 +0530

    user login and register actions added and initial state initialized in store

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,5 +1,29 @@
 import axios from "axios"
-import { USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants"
+
+export const login = (email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+        })
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/users/login', { email, password }, config)
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: res.data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(res.data))
+    } catch (error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
 
 export const register = (name, email, password) => async (dispatch) => {
     try {
@@ -16,8 +40,15 @@ export const register = (name, email, password) => async (dispatch) => {
             type: USER_REGISTER_SUCCESS,
             payload: res.data
         })
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: res.data
+        })
+        localStorage.setItem('userInfo', JSON.stringify(res.data) )
     } catch (error) {
-        
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message 
+        })
     }
-
 } 
