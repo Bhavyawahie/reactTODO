a7dfb82183a7a9cbf94d736e862c41b98bb6c25b
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-21 03:14:31 +0530

    note screen and note fetching from backend added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,39 @@
-import React from "react"
+import React, {useState} from "react"
+import {Route, Link} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import AppsIcon from '@material-ui/icons/Apps'
+import IconButton from "@material-ui/core/IconButton";
+import {Avatar, Button} from "@material-ui/core";
 
 const Header = () => {
-    return <header><h1>Keeper</h1></header>
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const {userInfo} = userLogin
+    const logoutHandler = () => {
+        console.log('i was clicked')
+    }
+    return (
+        <header>
+            <h1>Keeper</h1>
+            {
+            userInfo ? (
+                <div>
+                    <IconButton>
+                        <AppsIcon className='app-icon'/>
+                    </IconButton>
+                    <IconButton onClick={logoutHandler}>
+                        <Avatar>B</Avatar>
+                    </IconButton>
+                </div>
+            ) : (
+                <div>
+                    <Link to='/register'>
+                        <Button variant='contained' color='primary'>Sign Up</Button>
+
+                    </Link>
+                </div>
+            )
+        } </header>
+    )
 }
 export default Header
--- client/src/screens/loginScreen.jsx
+++ client/src/screens/loginScreen.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    Button,
+    Card,
+    Container,
+    Input,
+} from '@material-ui/core'
+import { login } from '../actions/userActions'
 
-const loginScreen = () => {
+
+const loginScreen = ({history, location}) => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+    const userLogin = useSelector(state => state.userLogin)
+    const {loading, error, userInfo} = userLogin
+    const redirect = location.seach ? location.search.split('=')[1] : '/note'
+    
+    useEffect(() => {
+        if(userInfo) {
+            history.push(redirect)
+        }
+    }, [userInfo, history, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+    }
+
+
     return (
-        <div>
-            login here
-        </div>
+        <Card>
+            <Container maxWidth="sm">
+                <form>
+                    <Input placeholder='email' type="email"
+                        name={email}
+                        value={email}
+                        onChange={
+                            (e) => setEmail(e.target.value)
+                        }/>
+                    <br/>
+                    <Input placeholder='password' type='password'
+                        name={password}
+                        value={password}
+                        onChange={
+                            (e) => setPassword(e.target.value)
+                        }/>
+                    <br/>
+                    <Button variant='contained' color='primary'
+                        onClick={submitHandler}>Log In</Button>
+                </form>
+            </Container>
+        </Card>
     )
 }
 
