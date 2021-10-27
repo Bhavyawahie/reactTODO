fccd2db53659f2a3692d61559140f017572feb73
Bhavya Wahie <bhavyawahie@gmail.com>
2021-10-28 02:11:18 +0530

    login feature made to work

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants";
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants";
 
 export const userRegisterReducer = (state = {}, action) => {
     switch (action.type) {
@@ -17,7 +17,7 @@ export const userLoginReducer = (state = {}, action) => {
     switch(action.type) {
         case USER_LOGIN_REQUEST:
             return { loading: true, ...state }
-        case USER_REGISTER_SUCCESS: 
+        case USER_LOGIN_SUCCESS: 
             return { loading: false, userInfo: action.payload }
         case USER_LOGIN_FAIL: 
             return { loading: false, error: action.payload }
