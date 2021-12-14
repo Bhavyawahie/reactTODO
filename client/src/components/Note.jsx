96ec2ce55ed5ca196fb84708ead7cc0a57996f90
Bhavya Wahie <bhavyawahie@gmail.com>
2021-12-14 15:50:22 +0530

    note updation modal form added on element onclick

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -1,15 +1,22 @@
 import React from "react"
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline'
 
-const Note = ({id, title, content, onDelClick}) => {
-    const delHandler = () => {
+const Note = ({id, title, content, onDelClick, onNoteClick, modelOpener}) => {
+    const delHandler = (e) => {
+        e.stopPropagation()
         onDelClick(id)
     }
 
+const dialogOpener = (e) => {
+    if (e.target.nodeName === 'DIV') {
+        onNoteClick(id,e)
+    }
+}    
+
     return ( 
-    <div className="note">
+    <div className="note" onClick={dialogOpener}>
         <h1>{title}</h1>
-        <p>{content}</p>
+        <p>{content.length <  150 ?  content : content.substring(0,150) + "..."}</p>
         <button onClick={delHandler}><DeleteOutlineIcon/></button>
     </div>
     );
