ee551790183f746f3a6a83e95af114a9c42b0c5a
Bhavya Wahie <bhavyawahie@gmail.com>
2021-12-14 15:49:22 +0530

    state management for note updation created

--- client/src/actions/noteActions.js
+++ client/src/actions/noteActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { NOTE_CREATE_FAIL, NOTE_CREATE_REQUEST, NOTE_CREATE_SUCCESS, NOTE_DELETE_FAIL, NOTE_DELETE_REQUEST, NOTE_DELETE_SUCCESS, NOTE_LIST_FAIL, NOTE_LIST_REQUEST, NOTE_LIST_SUCCESS } from '../constants/noteConstants'
+import { NOTE_CREATE_FAIL, NOTE_CREATE_REQUEST, NOTE_CREATE_SUCCESS, NOTE_DELETE_FAIL, NOTE_DELETE_REQUEST, NOTE_DELETE_SUCCESS, NOTE_LIST_FAIL, NOTE_LIST_REQUEST, NOTE_LIST_SUCCESS, NOTE_UPDATE_FAIL, NOTE_UPDATE_REQUEST, NOTE_UPDATE_SUCCESS } from '../constants/noteConstants'
 
 
 export const listNotes = () => async (dispatch, getState) => {
@@ -46,7 +46,7 @@ export const createNote = (input) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: NOTE_CREATE_FAIL,
-            payload: error.reponse && error.response.data.message ? error.response.data.message : error.message
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
 }
@@ -71,7 +71,32 @@ export const deleteNote = (id) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: NOTE_DELETE_FAIL,
-            payload: error.reponse && error.response.data.message ? error.response.data.message : error.message
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
+        })
+    }
+}
+
+export const updateNote = (input, id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: NOTE_UPDATE_REQUEST
+        })
+        const { userLogin : { userInfo } } = getState()
+        const config = {
+            headers : {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+        const res = await axios.put(`/api/notes/${id}`, input, config)  
+        dispatch({
+            type: NOTE_UPDATE_SUCCESS,
+            payload: res.data
+        })
+    } catch (error) {
+        dispatch({
+            type: NOTE_UPDATE_FAIL,
+            payload: error.response && error.response.data.message ? error.response.data.message : error.message
         })
     }
 }
