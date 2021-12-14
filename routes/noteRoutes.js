0a9c3cdb5e55f8f85398b018bc1d5c4db537d5aa
Bhavya Wahie <bhavyawahie@gmail.com>
2021-12-14 15:48:14 +0530

    note put request controller function and route added

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router()
-const {getNotes, addNotes, deleteNotes} = require('../controllers/notesController');
+const {getNotes, addNotes, deleteNotes, editNotes} = require('../controllers/notesController');
 const { protect } = require('../middlewares/authMiddleware');
 
 router.route("/").get(protect, getNotes).post(protect, addNotes)
-router.delete("/:id", protect, deleteNotes )
+router.route("/:id").put(protect, editNotes).delete( protect, deleteNotes )
 
 module.exports = router
