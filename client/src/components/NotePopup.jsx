50e266d2899d8f79b5abf0972dbcef9bf8136deb
Bhavya Wahie <bhavyawahie@gmail.com>
2022-06-24 02:01:23 +0530

    editing note made to work with State from currentNote

--- client/src/components/NotePopup.jsx
+++ client/src/components/NotePopup.jsx
@@ -1,19 +1,15 @@
-import {
-    Button,
-    Dialog,
-    Grid,
-    Input,
-    InputBase,
-    Typography
-} from '@material-ui/core'
+import { Button, Dialog, Grid, Input, InputBase, Typography } from '@material-ui/core'
 import React, {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
 
-const NotePopup = ({open, note, onClose, onSave}) => {
+const NotePopup = ({open, onClose, onSave}) => {
     const [noteBody, setNoteBody] = useState({
         title: "",
         content: ""
     })
+    const noteCurrentSet = useSelector(state => state.noteCurrentSet)
+    const {currentNote} = noteCurrentSet
 
     const changeHandler = (e) => {
         const {name, value} = e.target
@@ -26,13 +22,13 @@ const NotePopup = ({open, note, onClose, onSave}) => {
 
 
     return (
-        <Dialog scroll='paper' open={open} onClose={onClose}>
+        <Dialog scroll='paper' open={open} onClose={onClose} >
             <Grid>
-                    <InputBase placeholder='Title' name='title' onChange={changeHandler} defaultValue={note.title} className='popup-title' multiline fullWidth/>
-                    <InputBase placeholder='Note' name='content' onChange={changeHandler} defaultValue={note.content} className='popup-content' multiline fullWidth/>
+                    <InputBase placeholder='Title' name='title' onChange={changeHandler} defaultValue={currentNote && currentNote.title} className='popup-title' inputProps={{style: {fontWeight: 700, fontSize: 24}}} multiline fullWidth/>
+                    <InputBase placeholder='Note' name='content' onChange={changeHandler} defaultValue={currentNote && currentNote.content} className='popup-content' multiline fullWidth/>
             </Grid>
             <Grid container justifyContent='flex-end' style={{margin: 0,width: '100%'}}>
-                <Button variant='outlined' onClick={() => {onSave(noteBody, note.id); setNoteBody({title: "", content: ""});onClose()}}>Save</Button>
+                <Button variant='outlined' onClick={() => {onSave(noteBody, currentNote.id); setNoteBody({title: "", content: ""});onClose()}}>Save</Button>
             </Grid>
         </Dialog>
     )
