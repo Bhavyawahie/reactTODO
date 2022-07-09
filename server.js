af71bccefd99d1ffa9973a9448f75347f3d0f3b6
Bhavya Wahie <bhavyawahie@gmail.com>
2022-07-10 05:10:38 +0530

    server and gitignore updated for deployment

--- server.js
+++ server.js
@@ -26,15 +26,15 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
 
+
+app.use("/api/notes", noteRoutes)
+app.use("/api/users", userRoutes)
 if(process.env.NODE_ENV === "production" ){
     app.use(express.static("client/build"))
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
-
-app.use("/api/notes", noteRoutes)
-app.use("/api/users", userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
